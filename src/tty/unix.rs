d06f795884ae9f32ab5768759a964a3683f570ec
Ariel Mashraki <ariel@mashraki.co.il>
2015-10-14 13:54:14 +0300

    test(tty/unix): fix test on mac

--- src/tty/unix.rs
+++ src/tty/unix.rs
@@ -41,3 +41,31 @@ pub fn terminal_size() -> Option<(Width, Height)> {
         None
     }
 }
+
+#[test]
+/// Compare with the output of `stty size`
+fn compare_with_stty() {
+    use std::process::Command;
+    use std::process::Stdio;
+    let mut args = vec!["-F", "/dev/stderr", "size"];
+    if cfg!(target_os = "macos") {
+        args[0] = "-f"
+    }
+    let output = Command::new("stty")
+                         .args(&args)
+                         .stderr(Stdio::inherit()).output().unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+     assert!(output.status.success());
+
+    // stdout is "rows cols"
+    let mut data = stdout.split_whitespace();
+    let rows = u16::from_str_radix(data.next().unwrap(), 10).unwrap();
+    let cols = u16::from_str_radix(data.next().unwrap(), 10).unwrap();
+    println!("{}", stdout);
+    println!("{} {}", rows, cols);
+
+    if let Some((Width(w), Height(h))) = terminal_size() {
+        assert_eq!(rows, h);
+        assert_eq!(cols, w);
+    }
+}
