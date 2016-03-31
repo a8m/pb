586b03136a0af3bcd0ed8b08f9ff84f71262e6c6
Steven Fackler <sfackler@gmail.com>
2016-03-30 21:50:45 -0700

    Use winsize from libc

--- src/tty/unix.rs
+++ src/tty/unix.rs
@@ -1,20 +1,11 @@
 extern crate libc;
 use super::{Width, Height};
-use std::os::raw::*;
-
-#[derive(Debug)]
-struct WinSize {
-    ws_row: c_ushort,
-    ws_col: c_ushort,
-    ws_xpixel: c_ushort,
-    ws_ypixel: c_ushort,
-}
 
 /// Returns the size of the terminal, if available.
 ///
 /// If STDOUT is not a tty, returns `None`
 pub fn terminal_size() -> Option<(Width, Height)> {
-    use self::libc::{ioctl, isatty, STDOUT_FILENO, TIOCGWINSZ};
+    use self::libc::{ioctl, isatty, STDOUT_FILENO, TIOCGWINSZ, winsize};
     let is_tty: bool = unsafe { isatty(STDOUT_FILENO) == 1 };
 
     if !is_tty {
@@ -22,7 +13,7 @@ pub fn terminal_size() -> Option<(Width, Height)> {
     }
 
     let (rows, cols) = unsafe {
-        let mut winsize = WinSize {
+        let mut winsize = winsize {
             ws_row: 0,
             ws_col: 0,
             ws_xpixel: 0,
