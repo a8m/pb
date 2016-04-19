a7d62e5cd3e3ff226e9d11d72779b37cc7a6ec5a
althonos <althonosdev@gmail.com>
2016-04-19 08:59:52 +0200

    Added a loading symbol (tick) and the possibility to display a message at the beginning of the line
    Added default tick_format,
    corrected the api so that apps using old version of library are not forced to display a tick,
    corrected typos in documentation
    corrected format with rustfmt

--- tests/lib.rs
+++ tests/lib.rs
@@ -23,3 +23,59 @@ fn simple_iter_example(){
     }
     println!("done!");
 }
+
+#[test]
+fn timeout_example() {
+    let count = 100;
+    let mut pb = ProgressBar::new(count*20);
+    pb.tick_format("▏▎▍▌▋▊▉██▉▊▋▌▍▎▏");
+    pb.show_message = true;
+    pb.inc();
+    for _ in 0..count {
+        for _ in 0..20 {
+            pb.message("Waiting  : ");
+            thread::sleep(Duration::from_millis(50));
+            pb.tick();
+        }
+        for _ in 0..20 {
+            pb.message("Connected: ");
+            thread::sleep(Duration::from_millis(50));
+            pb.inc();   
+        }
+    }
+    for _ in 0..10 {
+        pb.message("Cleaning :");
+        thread::sleep(Duration::from_millis(50));
+        pb.tick();
+    }
+    pb.finish();
+    println!("done!");
+}
+
+#[test]
+fn npm_bar() {
+   let count = 20;
+    let mut pb = ProgressBar::new(count*5);
+    pb.tick_format("\\|/-");
+    pb.format("|#--|");
+    pb.show_tick = true;
+    pb.show_speed = false;
+    pb.show_percent = false;
+    pb.show_counter = false;
+    pb.show_time_left = false;
+    pb.inc();
+    for _ in 0..count {
+        for _ in 0..5 {
+            pb.message("normalize -> thing ");
+            thread::sleep(Duration::from_millis(80));
+            pb.tick();
+        }
+        for _ in 0..5 {
+            pb.message("fuzz -> tree       ");
+            thread::sleep(Duration::from_millis(80));
+            pb.inc();   
+        }
+    }
+    pb.finish();
+    println!("done!");
+}
