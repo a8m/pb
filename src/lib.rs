ea409fab32ebaf66cc134780b0749b95c78a1d34
Ariel Mashraki <ariel@mashraki.co.il>
2016-08-21 22:09:58 +0300

    fix: docs heading

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! # Console progress bar for Rust
 //!
 //! Console progress bar for Rust Inspired from [pb](http://github.com/cheggaaa/pb).
-//! support and tested on MacOS and Linux(should work on Windows too, but not tested yet).
+//! support and tested on MacOS, Linux and Window
 //!
 //! ![Screenshot](https://raw.githubusercontent.com/a8m/pb/master/gif/rec_v2.gif)
 //!
