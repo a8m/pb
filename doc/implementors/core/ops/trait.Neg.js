78e3a5552ed562d70b3f1eda9dc2d4a61e6fe7f3
Ariel Mashraki <ariel@mashraki.co.il>
2016-08-21 22:03:51 +0300

    update docs

--- doc/implementors/core/ops/trait.Neg.js
+++ doc/implementors/core/ops/trait.Neg.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];
+implementors['libc'] = [];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='time/struct.Duration.html' title='time::Duration'>Duration</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/std/error/trait.Error.js
+++ doc/implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='time/enum.ParseError.html' title='time::ParseError'>ParseError</a>",];implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='time/enum.ParseError.html' title='time::ParseError'>ParseError</a>",];
+implementors['time'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='time/enum.ParseError.html' title='time::ParseError'>ParseError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
