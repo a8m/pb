8e0c6839f6b080aaae3311d301a2fc8aa9cebce9
Ariel Mashraki <ariel@mashraki.co.il>
2016-10-27 10:38:57 +0300

    examples: add simple

---
+++ examples/simple.rs
@@ -0,0 +1,18 @@
+extern crate rand;
+extern crate pbr;
+use rand::Rng;
+use pbr::ProgressBar;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let count = 500;
+    let mut pb = ProgressBar::new(count);
+    pb.format("╢▌▌░╟");
+    for _ in 0..count {
+        pb.inc();
+        let n = rand::thread_rng().gen_range(0, 100);
+        thread::sleep(Duration::from_millis(n));
+    }
+    pb.finish_println("done!");
+}
