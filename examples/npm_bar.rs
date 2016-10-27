1affc5954411db7984616c63d42b05c1d78e97a6
Ariel Mashraki <ariel@mashraki.co.il>
2016-10-27 10:47:48 +0300

    examples: add npm_bar example

---
+++ examples/npm_bar.rs
@@ -0,0 +1,30 @@
+extern crate pbr;
+use pbr::ProgressBar;
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let count = 30;
+    let mut pb = ProgressBar::new(count * 10);
+    pb.tick_format("\\|/-");
+    pb.format("|#--|");
+    pb.show_tick = true;
+    pb.show_speed = false;
+    pb.show_percent = false;
+    pb.show_counter = false;
+    pb.show_time_left = false;
+    pb.inc();
+    for _ in 0..count {
+        for _ in 0..10 {
+            pb.message("normalize -> thing ");
+            thread::sleep(Duration::from_millis(80));
+            pb.tick();
+        }
+        for _ in 0..10 {
+            pb.message("fuzz -> tree       ");
+            thread::sleep(Duration::from_millis(80));
+            pb.inc();
+        }
+    }
+    pb.finish_println("done!");
+}
