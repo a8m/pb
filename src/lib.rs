e8003919ed4ad5aef55045770270b5cc2a32cbc4
Ariel Mashraki <ariel@mashraki.co.il>
2016-10-30 18:41:48 +0200

    docs: fix gif url

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@
 //! Console progress bar for Rust Inspired from [pb](http://github.com/cheggaaa/pb), support and
 //! tested on MacOS, Linux and Windows
 //!
-//! ![Screenshot](https://github.com/a8m/pb/blob/master/gif/rec_v3.gif)
+//! ![Screenshot](https://raw.githubusercontent.com/a8m/pb/master/gif/rec_v3.gif)
 //!
 //! [Documentation](http://a8m.github.io/pb/doc/pbr/index.html)
 //!
