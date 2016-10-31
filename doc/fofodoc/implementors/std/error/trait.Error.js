5a9730b81f7b4295f34dda44fbdd578408a6f275
Ariel Mashraki <ariel@mashraki.co.il>
2016-10-31 11:49:25 +0200

    update docs

---
+++ doc/fofodoc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["time"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='struct' href='time/struct.OutOfRangeError.html' title='time::OutOfRangeError'>OutOfRangeError</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='time/enum.ParseError.html' title='time::ParseError'>ParseError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
