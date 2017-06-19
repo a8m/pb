43ecff5a013a314dfe77ce0c3dc21777caf71a07
Ian Douglas Scott <ian@iandouglasscott.com>
2017-06-18 18:32:11 -0700

    Redox OS support

---
+++ src/tty/redox.rs
@@ -0,0 +1,13 @@
+extern crate termion;
+use super::{Width, Height};
+
+pub fn terminal_size() -> Option<(Width, Height)> {
+    match termion::terminal_size() {
+        Ok((cols, rows)) => Some((Width(cols), Height(rows))),
+        Err(..) => None
+    }
+}
+
+pub fn move_cursor_up(n: usize) -> String {
+    format!("{}", termion::cursor::Up(n as u16))
+}
