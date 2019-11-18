a6654e1c5fb41db53cd2bc8ec6e06f2adf02f388
Maksym Vorobiov <maxim.vorobjov@gmail.com>
2019-11-18 23:24:01 +0200

    add elementary wasi support

---
+++ src/tty/wasi.rs
@@ -0,0 +1,16 @@
+extern crate libc;
+use super::{Width, Height};
+
+/// For WASI so far it will return none
+///
+/// For background https://github.com/WebAssembly/WASI/issues/42
+pub fn terminal_size() -> Option<(Width, Height)> {
+    return None;
+}
+
+/// This is inherited from unix and will work only when wasi executed on unix.
+/// 
+/// For background https://github.com/WebAssembly/WASI/issues/42
+pub fn move_cursor_up(n: usize) -> String {
+    format!("\x1B[{}A", n)
+}
