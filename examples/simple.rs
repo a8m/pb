3d106e844b9b6c19706e2141b7710f92d046fc09
LingMan <LingMan@users.noreply.github.com>
2022-06-21 06:41:20 +0200

    Update `rand` dev-dependency to v0.8

--- examples/simple.rs
+++ examples/simple.rs
@@ -9,7 +9,7 @@ fn main() {
     pb.format("╢▌▌░╟");
     for _ in 0..count {
         pb.inc();
-        let n = thread_rng().gen_range(0, 100);
+        let n = thread_rng().gen_range(0..100);
         thread::sleep(Duration::from_millis(n));
     }
     pb.finish_println("done!");
